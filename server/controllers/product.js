d03394969fe68b82a7d5b880a55a244f5182ad62
Salvatore Dininni <s.dininni@yahoo.com>
2023-03-15 08:07:45 +0100

    best seller and new arrivals component implementation

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -68,3 +68,20 @@ exports.update = async (req, res) => {
     });
   }
 };
+
+exports.list = async (req, res) => {
+  try {
+    //createAT/updatedAt, desc/asc, 3
+    const { sort, order, limit } = req.body;
+    const products = await Product.find({})
+      .populate('category')
+      .populate('subs')
+      .sort([[sort, order]])
+      .limit(limit)
+      .exec();
+
+    res.json(products);
+  } catch (error) {
+    console.log(err);
+  }
+}
--- server/routes/product.js
+++ server/routes/product.js
@@ -7,6 +7,7 @@ const { authCheck, adminCheck } = require('../middlewares/auth');
 // controller
 const { 
   create, 
+  list,
   listAll, 
   remove, 
   read, 
@@ -19,5 +20,7 @@ router.get('/products/:count', listAll);
 router.delete('/product/:slug',authCheck ,adminCheck, remove);
 router.get('/product/:slug', read);
 router.put('/product/:slug', authCheck, adminCheck, update);
+
+router.post('/products', list);
 
 module.exports = router;
