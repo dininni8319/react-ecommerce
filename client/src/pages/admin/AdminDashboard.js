e9c2d423c28acd2c808e7a159ebc83ce086b310c
Salvatore Dininni <s.dininni@yahoo.com>
2023-03-29 22:39:10 +0200

    implementations for the user purchase history and admin dashboard orders

--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -1,8 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AdminNav from "../../components/nav/AdminNav";
+import { getOrders, changeStatus } from "../../functions/admin";
+import { useSelector, useDispatch } from 'react-redux';
+import { toast } from "react-toastify";
+import Orders from '../../components/order/Orders';
 
 const AdminDashboard = () => {
-  
+  const [ orders, setOrders ] = useState([]);
+  const { user } = useSelector((state) => ({...state}));
+
+  useEffect(() => {
+    loadOrders();
+  },[]);
+
+  const loadOrders =() => {
+    getOrders(user.token).then(res => {
+      console.log(JSON.stringify(res.data, null, 4));
+      setOrders(res.data);
+    });
+  };
+
+  const handleStatusChange = (orderId, orderStatus) => {
+    changeStatus(orderId, orderStatus, user.token).then(res => {
+      toast.success("Status updated");
+      loadOrders();
+    })
+  };
+
   return ( 
     <div className="container-fluid">
     <div className="row">
@@ -11,8 +35,9 @@ const AdminDashboard = () => {
       </div>
 
       <div className="col py-3">
-       
-        <h4>Admin AdminDashboard</h4>
+       <h4>Admin AdminDashboard</h4>
+       {/* {JSON.stringify(orders)} */}
+       <Orders orders={orders} handleStatusChange={handleStatusChange} />
       </div>
     </div>
   </div>
